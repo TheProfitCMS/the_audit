d063dc1a1c8692d1484312b2dccb6c39187101b1
Denis Krivostchokov <densomart@gmail.com>
2014-01-09 17:49:34 +0700

    Add concerns files to gem by hand

--- lib/the_audit/engine.rb
+++ lib/the_audit/engine.rb
@@ -3,6 +3,7 @@ require 'rails'
 
 module TheAudit
   class Engine < Rails::Engine
+    #TODO taichiman: We need this lines? 
     # initializer "Assets precompile hook", :group => :all do |app|
     #   app.config.assets.precompile += %w( file.js file.css )
     # end
