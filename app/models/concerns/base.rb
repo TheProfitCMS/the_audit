bb1ed1f1803b415a8ca5552761154a7511e13a55
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-08-13 23:48:49 +0400

    ~~~

--- app/models/concerns/base.rb
+++ app/models/concerns/base.rb
@@ -114,7 +114,7 @@ module TheAudit
 
       if r = controller.request
         self.ip          = r.ip
-        self.user_agent  = r.user_agent
+        self.user_agent  = r.user_agent.to_s[0..250]
         self.bot         = TheAudit.is_bot?(r.user_agent)
 
         self.remote_ip   = r.remote_ip
